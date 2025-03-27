81e368a6267e38bfa80b58e1149b34ad9ddf6804
Gautam Tayal <gautamtayal65@gmail.com>
2025-03-27 15:10:44 +0530

    auth fixed

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,42 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs"
+import prisma from "@repo/db/config"
 
-const handler = NextAuth({
+const authOption: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
     })
   ],
-})
+  session: {
+    strategy: "jwt"
+  },
+  callbacks: {
+    async signIn({account, profile}) {
+      if(!profile?.email) {
+        throw new Error('no profile')
+      }
 
+      await prisma.user.upsert({
+        where: {
+          email: profile.email
+        },
+        create: {
+          email: profile.email,
+          name: profile.name
+        },
+        update: {
+          name: profile.name
+        }
+      })
+
+      return true
+    }
+  }
+}
+
+const handler = NextAuth(authOption)
 export { handler as GET, handler as POST }
--- apps/web/app/components/Navbar.tsx
+++ apps/web/app/components/Navbar.tsx
@@ -3,16 +3,21 @@
 import { signIn, signOut, useSession } from "next-auth/react"
 import { Button } from "./ui/button"
 
+
 export const Navbar = () => {
-  const session = useSession()
+  const {data: session} = useSession()
 
   return (
-    <div>
+    <div className="flex justify-between items-center p-3 bg-[#282a36] text-white">
       <h1 className="">LOGO</h1>
       <div>
-        {session?.data?.user 
+        {session?.user 
         ? 
+        <div className="flex items-center gap-3">
+          <p>Welcome, {session?.user?.name}</p>
           <Button onClick={() => {signOut()}}>Logout</Button>
+        </div>
+          
         :
           <Button onClick={() => {signIn()}}>Login</Button>}
       </div>
