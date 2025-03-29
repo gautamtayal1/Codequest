f46fbc77d836ca116948c419b23e056db362ad62
Gautam Tayal <gautamtayal65@gmail.com>
2025-03-30 01:09:46 +0530

    contest and problemms page ui

--- apps/boilerplate-generator/src/index.ts
+++
@@ -1,5 +0,0 @@
-import generateAllBoilerplate from "./generatePartialBoilerplate";
-import generateFullBoilerplate from "./generteFullBoilerplate";
-
-generateAllBoilerplate().catch(err => console.error("error: ", err)) 
-generateFullBoilerplate().catch(err => console.error("error: ", err)) 
