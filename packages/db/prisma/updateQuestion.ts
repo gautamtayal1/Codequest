ec07bfe39c2ff92bf2f8dd7beef0345b5414460b
Gautam Tayal <gautamtayal65@gmail.com>
2025-03-30 17:03:58 +0530

    update question in db logic

---
+++ packages/db/prisma/updateQuestion.ts
@@ -0,0 +1,54 @@
+import { promises as fs } from "fs"
+import prisma from "../src/index.js"
+
+export const LANGUAGE_MAPPING : {
+  [key: string]: {
+    judge0: number;
+    internal: number;
+    name: string;
+    monaco: string
+  }
+} = {
+  js: {judge0: 63, internal: 1, name: "Javascript", monaco: "javascript"},
+  cpp: {judge0: 54, internal: 2, name: "C++", monaco: "cpp"}
+}
+
+
+async function main(problemSlug: string, problemTitle: string) {
+  const problemStatement = await fs.readFile(`../../../apps/problems/${problemSlug}/Problem.md`, "utf8")
+
+  const problem = await prisma.problem.upsert({
+    where: { slug: problemSlug },
+    create: {
+      title: problemSlug,
+      slug: problemSlug,
+      description: problemStatement
+    },
+    update: {
+      description: problemStatement
+    }
+  })
+
+  await Promise.all(
+    Object.keys(LANGUAGE_MAPPING).map(async(language) => {
+      const code = await fs.readFile(`../../../apps/problems/${problemSlug}/boilerplate/function.${language}`, "utf8")
+
+      await prisma.defaultCode.upsert({
+        where: {
+          problemId_languageId: {
+            problemId: problem.id,
+            languageId: LANGUAGE_MAPPING[language]?.internal
+          }
+        },
+        create: {
+          problemId: problem.id,
+          languageId: LANGUAGE_MAPPING[language]?.internal,
+          code
+        },
+        update: {code}
+      })
+    })
+  )
+}
+
+main(process.env.PROBLEM_SLUG!, process.env.PROBLEM_TITLE!)
